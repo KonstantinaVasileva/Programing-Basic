ff85b6485507348f6bda3cedd2050a8b09944250
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2023-12-07 19:57:30 +0200

    Add files via upload

---
+++ First Steps In Coding/More Exercise/WeatherForecastPart2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class WeatherForecastPart2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double temperature = Double.parseDouble(scanner.nextLine());
+
+        if (temperature > 35)
+        {
+            System.out.println("unknown");
+        } else if (temperature >25.9)
+        {
+            System.out.println("Hot");
+
+        } else if (temperature >20)
+        {
+            System.out.println("Warm");
+        } else if (temperature > 14.9)
+        {
+            System.out.println("Mild");            
+        } else if (temperature > 11.9)
+        {
+         System.out.println("Cool");
+        } else if (temperature >= 5)
+        {
+            System.out.println("Cold");
+        } else
+        {
+            System.out.println("unknown");
+        }
+
+    }
+}
