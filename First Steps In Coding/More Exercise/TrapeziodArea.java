cd8824fb0188bdcbe82b07edc32399fb6c855a3a
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2023-12-07 19:55:44 +0200

    Add files via upload

---
+++ First Steps In Coding/More Exercise/TrapeziodArea.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TrapeziodArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = Double.parseDouble(scanner.nextLine());
+        double b = Double.parseDouble(scanner.nextLine());
+        double h = Double.parseDouble(scanner.nextLine());
+
+        double area = (a+b)/2 * h;
+
+        System.out.printf("%.2f",area);
+
+    }
+}
